44e47f8a77ca0bab98fa4f6439ed0e3e54413b5e
Aiswarya2316 <aiswaryapinkey@gmail.com>
2025-04-01 08:23:57 +0530

    completed the image designing

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from .views import AddItemView, ItemListView, SearchItemView, delete_item
 urlpatterns = [
     path('',views.home,name='home'),
-    path('add/', AddItemView.as_view(), name='add_item'),
-    path('items/', ItemListView.as_view(), name='item_list'),
-    path('search/', SearchItemView.as_view(), name='search_item'),
-    path('delete/<int:item_id>/', delete_item, name='delete_item'),
+    path('add_item/', AddItemView.as_view(), name='add_item'),
+    path('item_list/', ItemListView.as_view(), name='item_list'),
+    path('search_item/', SearchItemView.as_view(), name='search_item'),
+    path('delete_item/<int:item_id>/', delete_item, name='delete_item'),
 
 
 ]
